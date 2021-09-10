a99db6bf3517ec9d6718103ef1f6100f8415ca95
monicasalas86 <monicsalas167@gmail.com>
2021-09-10 14:51:55 -0500

    connected API data to render characters names

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledCharacter = styled.div`
+    border: 1px solid black;
+    /* width: 60%; */
+    height: 80vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .character-div {
+        border: 1px solid green;
+    }
+`
+
+export default function CharacterList({name}) {
+    return (
+        <StyledCharacter>
+            <div className='character-div'>
+                <h3>{name}</h3>
+                {/* <button className='character-id'>{id}</button> */}
+            </div>
+        </StyledCharacter>
+    )
+}
